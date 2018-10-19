0355202023d8f2963c79cd55d185fabb8846dc14
Infinite-eyes <cwm930215@gmail.com>
2018-10-19 15:57:57 +0800

    1.base dagger test

--- todoapp2/app/src/main/java/com/example/android/architecture/blueprints/todoapp/MainActivity.java
+++ todoapp2/app/src/main/java/com/example/android/architecture/blueprints/todoapp/MainActivity.java
@@ -3,11 +3,25 @@ package com.example.android.architecture.blueprints.todoapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.android.architecture.blueprints.todoapp.daggertest.A;
+import com.example.android.architecture.blueprints.todoapp.daggertest.DaggerMainComponent;
+
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    A a;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DaggerMainComponent.create().inject(this);
+
+
+        a.eat();
     }
 }
